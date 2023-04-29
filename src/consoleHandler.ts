9472ced91bce514aaca9d602317d8d4d903cc607
Conutik <khawaj.zaid@gmail.com>
2023-04-29 17:10:25 +0300

    Create consoleHandler.ts

---
+++ src/consoleHandler.ts
@@ -0,0 +1,16 @@
+import readline from "readline";
+
+export function setupConsoleInterface(ws: WebSocket) {
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+    });
+
+    rl.on('line', (input) => {
+        let info = JSON.stringify({
+            type: "chat",
+            data: `"${input}"`
+        })
+        ws.send(info)
+    })
+}
