381a1dbf49e026eb15e6ae233f814d132576c7c9
amongus <102772348+urasdbwd@users.noreply.github.com>
2023-11-04 21:12:06 -0700

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,9 @@
+sudo apt install curl
+curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash 
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+nvm install 20
+nvm use 20
+npm i
+echo "echo 'to stop BAF please hold control and click the letter C on your keyboard'\n cd auto-flipper \n npm start" > ../start.sh
